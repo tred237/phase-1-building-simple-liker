9114918a8519a70ff22d2b4fd1521f3b99b4077b
trevor dewindt <trevor.dewindt@gmail.com>
2022-08-14 19:37:56 -0600

    commit tree 3b4c5ad501731b543787328bbc18f1d0b2a7eaf8

--- main.js
+++ main.js
@@ -3,6 +3,29 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let likeList = document.querySelectorAll('.like-glyph');
+likeList.forEach(element => element.addEventListener('click', clickAction));
+
+function clickAction(element){
+  mimicServerCall()
+  .then(() => {
+    let heart = element.target.textContent;
+    if(heart === EMPTY_HEART){
+      element.target.textContent = FULL_HEART;
+      element.target.classList.add('activated-heart');
+    } else {
+      element.target.textContent = EMPTY_HEART;
+      element.target.classList.remove('activated-heart');
+    }
+  })
+  .catch(error => {
+    let modal = document.querySelector('#modal');
+    modal.classList.remove('hidden');
+    modal.children[1].textContent = error;
+  
+    setTimeout(() => modal.classList.add('hidden'), 3000);
+  })
+}
 
 
 
